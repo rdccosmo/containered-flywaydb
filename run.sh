a5199dc9846a62aab4a582ec09a5a93f5f6dc33d
rodrigo.cosme <rodrigo.cosme@incaper.es.gov.br>
2015-08-18 07:24:31 -0400

    add environment variable to controle database driver

--- run.sh
+++ run.sh
@@ -13,6 +13,7 @@ OPTIONS:
     DATABASE_PASSWORD
     DATABASE_PORT
     DATABASE_ENCODING
+    DATABASE_DRIVER
     MIGRATIONS_PATH      
 EOF
 }
@@ -21,7 +22,7 @@ export MIGRATIONS_PATH=${MIGRATIONS_PATH:-'/home/flyway/migrations'}
 export DATABASE_PORT=${DATABASE_PORT:-3306}
 export DATABASE_ENCODING=${DATABASE_ENCODING:-UTF-8}
 
-if [[ -z $DATABASE_NAME ]] || [[ -z $DATABASE_USER ]] || [[ -z $DATABASE_PASSWORD ]] || [[ -z $MIGRATIONS_PATH ]]; then
+if [[ -z $DATABASE_NAME ]] || [[ -z $DATABASE_USER ]] || [[ -z $DATABASE_PASSWORD ]] || [[ -z $DATABASE_DRIVER ]] || [[ -z $MIGRATIONS_PATH ]]; then
     usage
     exit 1
 fi
@@ -29,5 +30,5 @@ fi
 while if ! nmap -T5 -p $DATABASE_PORT -sT db | grep open | wc -l ;then continue; else break;fi do break; done
 
 
-flyway -url=jdbc:mysql://db:$DATABASE_PORT/$DATABASE_NAME -user=$DATABASE_USER -password=$DATABASE_PASSWORD -encoding=$DATABASE_ENCODING -locations=filesystem:$MIGRATIONS_PATH migrate
+flyway -url=jdbc:$DATABASE_DRIVER://db:$DATABASE_PORT/$DATABASE_NAME -user=$DATABASE_USER -password=$DATABASE_PASSWORD -encoding=$DATABASE_ENCODING -locations=filesystem:$MIGRATIONS_PATH migrate
 
